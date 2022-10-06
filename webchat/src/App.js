f25d3e43fdfe84286cb48a25e7c4f9ca465f419d
unknown <kevinmbuicode>
2022-10-06 14:15:29 +0300

    feat: change path to register

--- webchat/src/App.js
+++ webchat/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register/Register';
+import './style.scss';
+
 
 function App() {
+
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register/>}/>
+        <Route path="/chat" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
